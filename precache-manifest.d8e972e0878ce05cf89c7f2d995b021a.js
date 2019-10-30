81308430e8303088d73b799a232a4db9c10fde1d
Jonathan Mau <jmau949@gmail.com>
2019-10-30 11:50:44 -0500

    Updates

---
+++ precache-manifest.d8e972e0878ce05cf89c7f2d995b021a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96123e26bb1b36e84194e3e7bc7af94e",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "6152b027e9bce3e670fe",
+    "url": "/react-memory-game/static/css/main.31478cad.chunk.css"
+  },
+  {
+    "revision": "b44899efa66fade97188",
+    "url": "/react-memory-game/static/js/2.bd842754.chunk.js"
+  },
+  {
+    "revision": "6152b027e9bce3e670fe",
+    "url": "/react-memory-game/static/js/main.48a7a8f4.chunk.js"
+  },
+  {
+    "revision": "e7cce8f96413fdbde7a2",
+    "url": "/react-memory-game/static/js/runtime-main.b1273a96.js"
+  }
+]);
